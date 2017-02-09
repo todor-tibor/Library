9c63651ce986bbd6459b1002450bdf4b86f9eb0d
gallb <barna.gall@msg-systems.com>
2017-02-09 16:04:29 +0200

    trying to rebase

---
+++ wildfly-users-ejb/src/main/java/gallb/wildfly/users/ejb/UserBean.java
@@ -0,0 +1,142 @@
+package gallb.wildfly.users.ejb;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.jboss.logging.Logger;
+
+import gallb.wildfly.users.common.IUser;
+import model.User;
+
+
+
+@Stateless
+public class UserBean implements IUser {
+	@PersistenceContext(unitName = "WildflyUsers")
+	private EntityManager oEntityManager;
+	private Logger oLogger = Logger.getLogger(UserBean.class);
+
+	@Override
+	public List<User> getAll() {
+		try {
+			@SuppressWarnings("unchecked")
+			List<User> users = (List<User>) oEntityManager.createNamedQuery("User.findAll").getResultList();
+			return users;
+		} catch (PersistenceException e) {
+			oLogger.error(e);
+			EjbExeption.getCause(e);
+			throw new EjbExeption("Can't find users", e);
+		}
+	}
+
+	@Override
+	public User getById(int id) {
+		try {
+			User u = oEntityManager.find(User.class, id);
+			return u;
+		} catch (PersistenceException e) {
+			oLogger.error(e);
+			EjbExeption.getCause(e);
+			throw new EjbExeption("Can't find user with specifiel id: <" + id + ">", e);
+		}
+	}
+
+	@Override
+	public void store(User user) {
+		try {
+			int n = ((Number) oEntityManager.createNamedQuery("User.maxId").getSingleResult()).intValue();
+			user.setUuid(n + 1);
+			oEntityManager.persist(user);
+			oEntityManager.flush();
+		} catch (PersistenceException e) {
+			oLogger.error(e);
+			EjbExeption.getCause(e);
+			throw new EjbExeption("--------Could not insert user.", e);
+		}
+	}
+
+	@Override
+	public void remove(int id) {
+		try {
+			oEntityManager.clear();
+			User u = getById(id);
+			oEntityManager.remove(u);
+			oEntityManager.flush();
+		} catch (PersistenceException e) {
+			oLogger.error(e);
+			EjbExeption.getCause(e);
+			throw new EjbExeption("Can't delete user with specifield id: <" + id + ">", e);
+		}
+	}
+
+	@Override
+	public void update(User user) {
+		try {
+			oEntityManager.merge(user);
+			oEntityManager.flush();
+		} catch (PersistenceException e) {
+			oLogger.error(e);
+			EjbExeption.getCause(e);
+			throw new EjbExeption("Can't update user with specifield id: <" + user.getUuid() + ">", e);
+
+		}
+	}
+
+	@Override
+	public List<User> search(String name) {
+		try {
+			CriteriaBuilder cb = oEntityManager.getCriteriaBuilder();
+			CriteriaQuery<User> criteria = cb.createQuery(User.class);
+			Root<User> member = criteria.from(User.class);
+
+			criteria.select(member).where(cb.like(member.get("userName"), "%" + name + "%"));
+			return oEntityManager.createQuery(criteria).getResultList();
+		} catch (PersistenceException e) {
+			oLogger.error(e);
+			EjbExeption.getCause(e);
+			throw new EjbExeption("Can't find any user.", e);
+		}
+	}
+
+//	@Override
+//	public boolean login(String username, int role) {
+//		oLogger.warn("--------------------------" + role);
+//		try {
+//			CriteriaBuilder cb = oEntityManager.getCriteriaBuilder();
+//			CriteriaQuery<User> criteria = cb.createQuery(User.class);
+//			Root<User> member = criteria.from(User.class);
+//
+//			criteria.select(member).where(cb.equal(member.get("username"), username));
+//			User u = oEntityManager.createQuery(criteria).getSingleResult();
+//
+//			InitialContext jndi = new InitialContext();
+//			IRole oRoleBean = (IRole) jndi.lookup(IRole.jndiNAME);
+//
+//			Role r = oRoleBean.getRoleById(role);
+//			oLogger.warn("--------------------------" + r);
+//			if (u.getRoles().contains(r)) {
+//				oLogger.info("----------user loged in as " + r.getRole());
+//				return true;
+//			} else {
+//				oLogger.info(username + " can't log in as " + r.getRole());
+//			}
+//		} catch (PersistenceException e) {
+//			oLogger.error(e);
+//			EjbExeption.getCause(e);
+//			throw new EjbExeption("Invalid username, can't login.", e);
+//		} catch (NamingException e) {
+//			oLogger.error(e);
+//			throw new EjbExeption("Invalid role, can't login", e);
+//		}
+//		return false;
+//	}
+}
--- wildfly-users-web/src/main/java/gallb/wildfly/users/web/ManagedBean.java
+++ wildfly-users-web/src/main/java/gallb/wildfly/users/web/ManagedBean.java
@@ -28,14 +28,11 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 /**
  * @author gallb
- *
+ * User manager.
  */
 @Named("userbean")
 
-//@ApplicationScoped
-//@RequestScoped
 @SessionScoped
-//@ViewScoped
 public class ManagedBean implements Serializable{
 
 	/**
@@ -43,44 +40,14 @@ public class ManagedBean implements Serializable{
 	 */
 	private Logger oLogger = Logger.getLogger(ManagedBean.class);
 	private static final long serialVersionUID = -4702598250751689454L;
+	
 	@Inject
 	private IUser oUserBean;
 	
-	private IRole oRoleBean = null;
 	private List<User> userList =  new ArrayList<>();
-	private List<Role> roleList = null;
-	private String selectedUser = null;
-	private String selectedRole = null;
 	private User currentUser = null;
-	
-	
-	
-	public User getCurrentUser() {
-		return currentUser;
-	}
-	public void setCurrentUser(User currentUser) {
-		this.currentUser = currentUser;
-	}
-//	public void showRoles() {
-//		currentUser = getById(selectedUser);
-//		oLogger.info("Current user:   " + currentUser.getUserName() + " " +currentUser.getRoles().get(0));
-//	}
-	public String getSelectedRole() {
-		return selectedRole;
-	}
 
-	public void setSelectedRole(String selectedRole) {
-		this.selectedRole = selectedRole;
-	}
-
-	public String getSelectedUser() {
-		return selectedUser;
-	}
-
-	public void setSelectedUser(String selectedUser) {
-		this.selectedUser = selectedUser;
-	}
-
+	//Setting faces context messages.
 	public void error(String message) {
 		oLogger.info("**********************Error CALLED***************************");
 		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", message));
@@ -98,6 +65,7 @@ public class ManagedBean implements Serializable{
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Fatal!", message));
     }
 	
+<<<<<<< Upstream, based on origin/master
   //  @PostConstruct
     public void onPostConstruct() {
     	System.out.println("*************************************postconstruct");
@@ -108,6 +76,18 @@ public class ManagedBean implements Serializable{
 			this.error("Server internal error.");
 		}
     }
+=======
+	public List<User> getUserList() {
+		return userList;
+	}
+    
+    public User getCurrentUser() {
+		return currentUser;
+	}
+	public void setCurrentUser(User currentUser) {
+		this.currentUser = currentUser;
+	}
+>>>>>>> aca2b28 Data Table v1
     
 	public List<User> getAll() {
 		oLogger.info("--getAllUsers()--");
@@ -115,12 +95,17 @@ public class ManagedBean implements Serializable{
 		try {
 			oLogger.info("--getAllUsers()--users queried");
 			userList = oUserBean.getAll();
+<<<<<<< Upstream, based on origin/master
 			//userList = getUserBean().getAll();
 		} catch (LibraryException e) {
+=======
+		} catch (BeanException e) {
+>>>>>>> aca2b28 Data Table v1
 			this.error("Server internal error.");
 		}
 		return userList;
 	}
+<<<<<<< Upstream, based on origin/master
 	
 	public List<Role> getAllRoles() {
 		oLogger.info("--getAllUsers()--");
@@ -197,14 +182,21 @@ public class ManagedBean implements Serializable{
 //		}
 //		return false;
 //	}
+=======
+>>>>>>> aca2b28 Data Table v1
 
 	public List<User> search(String p_searchTxt) {
 		oLogger.info("--search user--" + p_searchTxt);
 		if (p_searchTxt.length() >= 3) {
 			userList = new ArrayList<>();
 			try {
+<<<<<<< Upstream, based on origin/master
 				userList = getUserBean().search(p_searchTxt);
 			} catch (LibraryException e) {
+=======
+				userList = oUserBean.search(p_searchTxt);
+			} catch (BeanException e) {
+>>>>>>> aca2b28 Data Table v1
 				this.error(e.getMessage());
 			}
 		} else {
@@ -212,66 +204,49 @@ public class ManagedBean implements Serializable{
 		}
 		return userList;
 	}
+	public void store(String p_value) {
+		oLogger.info("--store user--");
+		oLogger.info("--store param: " + p_value);
+		if (p_value.isEmpty()) {
+			this.error("Empty field");
+		}
+		if (p_value == "") {
+			this.error("Empty field");
+		}
+		try {
+			User tmpUser = new User();
+			tmpUser.setUserName(p_value);
+			oUserBean.store(tmpUser);
+			userList.add(tmpUser);
+			this.info("Succesfully added: " + p_value);
+		} catch (BeanException e) {
+			this.error(e.getMessage());
+		}
+	}
 	
-	public List<Role> searchRole(String p_searchTxt) {
-		oLogger.info("--search Role--" + p_searchTxt);
-		if (p_searchTxt.length() >= 3) {
-			roleList = new ArrayList<>();
+	public void update(String p_newTxt) {
+		oLogger.info("--update user ManagedBean--id:" + currentUser.getUserName() + "new name: " +p_newTxt);
+		if ((currentUser != null) && (p_newTxt.length() >= 3)) {
 			try {
+<<<<<<< Upstream, based on origin/master
 				roleList = getRoleBean().search(p_searchTxt);
 			} catch (LibraryException e) {
+=======
+				currentUser.setUserName(p_newTxt);
+				oUserBean.update(currentUser);
+				userList = oUserBean.getAll(); 
+				oLogger.info("**********************update succesfull************************************");
+				this.info("Update succesfull.");
+			} catch (BeanException e) {
+				oLogger.error(e);
+>>>>>>> aca2b28 Data Table v1
 				this.error(e.getMessage());
-			}			
+			}
 		} else {
-			this.error("Keyword too short. Min. 3 characters req.");
-		}	
-		return roleList;
+			this.error("New name too short.");
+		}
 	}
 	
-	public List<User> getUserList() {
-		return userList;
-	}
-	
-	public List<Role> getRoleList() {
-		return roleList;
-	}
-
-//	public boolean update(String p_newTxt) {
-//		oLogger.info("--update user ManagedBean--id:" + selectedUser + "new name: " +p_newTxt);
-//		if ((selectedUser != null) && (p_newTxt.length() >= 3)) {
-//			//if ()
-//			try {
-//				getUserBean().update(selectedUser, p_newTxt);
-//				userList = getUserBean().getAll(); 
-//				oLogger.info("**********************update succesfull************************************");
-//				this.info("Update succesfull.");
-//			} catch (BeanException e) {
-//				oLogger.error(e);
-//				this.error(e.getMessage());
-//			}
-//		} else {
-//			this.error("New name too short.");
-//		}
-//		return true;
-//	}
-	
-//	public boolean updateRole(String p_newTxt) {
-//		oLogger.info("--update role ManagedBean--id:" + selectedRole + "new name: " +p_newTxt);
-//		if ((selectedRole != null) && (p_newTxt.length() >= 3)) {
-//			try {
-//				getRoleBean().update(selectedRole, p_newTxt);
-//				getRoleBean().getAll();
-//				oLogger.info("**********************update succesfull************************************");
-//				this.info("Update succesfull.");
-//			} catch (BeanException e) {
-//				oLogger.error(e);
-//				this.error(e.getMessage());
-//			}
-//		} else {
-//			this.error("New name too short.");
-//		}
-//		return true;
-//	}
 
 //	public boolean remove() {
 //		oLogger.info("--remove user by Id ManagedBean--p_id: " + selectedUser);
@@ -290,85 +265,4 @@ public class ManagedBean implements Serializable{
 //		}
 //		return true;
 //	}
-	
-//	public boolean removeRole() {
-//		oLogger.info("--remove role by Id ManagedBean--p_id: " + selectedRole);
-//		if (selectedRole == "") {
-//			this.error("Empty field");
-//			return false;
-//		} else {
-//			try {
-//				getRoleBean().remove(selectedRole);
-//				roleList = getRoleBean().getAll(); 
-//				this.info("Delete succesfull.");
-//			} catch (BeanException e) {
-//				oLogger.error(e);
-//				this.error(e.getMessage());
-//			}
-//		}
-//		return true;
-//	}
-
-//	public User getById(String p_id) {
-//		oLogger.info("--search user by Id ManagedBean--");
-//		try {
-//			return getUserBean().getById(p_id);
-//		}  catch (BeanException e) {
-//			oLogger.error(e);
-//			this.error(e.getMessage());
-//		}
-//		return null;
-//	}
-//	
-//	public Role getRoleById(String p_id) {
-//		oLogger.info("--search user by Id ManagedBean--");
-//		try {
-//			return getRoleBean().getById(p_id);
-//		}  catch (BeanException e) {
-//			oLogger.error(e);
-//			this.error(e.getMessage());
-//		}
-//		return null;
-//	}
-//	
-//	public boolean assignRole() {
-//		oLogger.info("--assign role ManagedBean--");
-//		if (selectedUser == null) {
-//			this.error("No user selected.");
-//			return false;
-//		}
-//		if (selectedRole == null) {
-//			this.error("No role selected.");
-//			return false;
-//		}
-//		try {
-//			getUserBean().addRole(selectedUser, selectedRole);
-//			this.info("Succesfully assigned.");
-//		} catch (BeanException e) {
-//			oLogger.error(e);
-//			this.error(e.getMessage());
-//		}
-//		return true;
-//	}
-//	
-//	public boolean revokeRole() {
-//		oLogger.info("--revoke role ManagedBean--");
-//		if (selectedUser == null) {
-//			this.error("No user selected.");
-//			return false;
-//		}
-//		if (selectedRole == null) {
-//			this.error("No role selected.");
-//			return false;
-//		}
-//		try {
-//			getUserBean().removeRole(selectedUser, selectedRole);
-//			this.info("Succesfully revoked.");
-//		} catch (BeanException e) {
-//			oLogger.error(e);
-//			this.error(e.getMessage());
-//		}
-//		return true;
-//	}
-	
 }
