948c40bf6f3fcb6af73493834418644d4ac12d95
Anna Katalin Kis <Katalin.Kis@msg-systems.com>
2017-02-17 12:04:09 +0200

    user and login untested

--- library-ejb/src/main/java/com/edu/library/access/userManagement/UserManagementFacade.java
+++ library-ejb/src/main/java/com/edu/library/access/userManagement/UserManagementFacade.java
@@ -2,11 +2,13 @@ package com.edu.library.access.userManagement;
 
 import java.util.List;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 import com.edu.library.IUserService;
-import com.edu.library.LibraryException;
+import com.edu.library.business.userManagement.UserManagementBusiness;
 import com.edu.library.model.User;
+import com.edu.library.util.ServiceValidation;
 
 /**
  * @author kiska Implements the basics of user login. Validates the given the
@@ -14,47 +16,50 @@ import com.edu.library.model.User;
  */
 @Stateless
 public class UserManagementFacade implements IUserService {
+	@EJB
+	private UserManagementBusiness userManagementBusiness;
 
 	@Override
-	public List<User> getAll() throws LibraryException {
-		// TODO Auto-generated method stub
-		return null;
+	public List<User> getAll() {
+		return userManagementBusiness.getAll();
 	}
 
 	@Override
-	public List<User> search(String p_searchTxt) throws LibraryException {
-		// TODO Auto-generated method stub
-		return null;
+	public List<User> search(String searchUserName) {
+		ServiceValidation.checkString(searchUserName);
+		return userManagementBusiness.search(searchUserName);
 	}
 
 	@Override
-	public User getById(String p_id) throws LibraryException {
-		// TODO Auto-generated method stub
-		return null;
+	public User getById(String user_id) {
+		ServiceValidation.checkUuid(user_id);
+		return userManagementBusiness.getById(user_id);
 	}
 
 	@Override
-	public void store(User p_value) throws LibraryException {
-		// TODO Auto-generated method stub
-		
+	public void store(User user) {
+		ServiceValidation.checkNotNull(user);
+		userManagementBusiness.store(user);
+
 	}
 
 	@Override
-	public void update(User p_user) throws LibraryException {
-		// TODO Auto-generated method stub
-		
+	public void update(User user) {
+		ServiceValidation.checkNotNull(user);
+		userManagementBusiness.update(user);
 	}
 
 	@Override
-	public void remove(String p_id) throws LibraryException {
-		// TODO Auto-generated method stub
-		
+	public void remove(String user_id) {
+		ServiceValidation.checkUuid(user_id);
+		userManagementBusiness.remove(user_id);
+
 	}
 
 	@Override
-	public User getByUserName(String userName) throws LibraryException {
-		// TODO Auto-generated method stub
-		return null;
+	public User getByUserName(String userName) {
+		ServiceValidation.checkString(userName);
+		return userManagementBusiness.searchForUserName(userName);
 	}
-	
+
 }
---
+++ library-ejb/src/main/java/com/edu/library/business/userManagement/UserManagementBusiness.java
@@ -0,0 +1,64 @@
+package com.edu.library.business.userManagement;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+import com.edu.library.LibraryException;
+import com.edu.library.business.exception.BusinessException;
+import com.edu.library.business.exception.ErrorMessages;
+import com.edu.library.data.userManagement.UserDao;
+import com.edu.library.model.User;
+
+/**
+ * @author kiska
+ * 
+ *         Business implementation of user management.
+ */
+
+@Stateless
+@LocalBean
+public class UserManagementBusiness {
+	@EJB
+	private UserDao dataAcces;
+
+	public List<User> getAll() {
+		return dataAcces.getAll();
+	}
+
+	public List<User> search(String userName) throws LibraryException {
+		return dataAcces.search(userName);
+	}
+
+	public User getById(String userId) throws LibraryException {
+		return dataAcces.getById(userId);
+	}
+
+	public void store(User user) throws LibraryException {
+		if (dataAcces.getByUserName(user.getUserName()).getUserName().isEmpty()) {
+			dataAcces.store(user);
+		} else {
+			throw new BusinessException(ErrorMessages.ERROR_CONSTRAINT_VIOLATION);
+		}
+	}
+
+	public void update(User user) throws LibraryException {
+		dataAcces.update(user);
+	}
+
+	public void remove(String userId) {
+		User pub = dataAcces.getById(userId);
+		if (pub.getBorrows().isEmpty()) {
+			dataAcces.remove(pub);
+		} else {
+			throw new BusinessException(ErrorMessages.ERROR_BOUND);
+		}
+	}
+
+	public User searchForUserName(String userName) {
+		return dataAcces.getByUserName(userName);
+	}
+
+}
