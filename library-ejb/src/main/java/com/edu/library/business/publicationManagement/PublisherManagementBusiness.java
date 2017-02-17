01148a6e6f3edfcb8d91f0fafa92cf6091303f2a
Nagy Szilard <nagy_szilardsandor@example.com>
2017-02-17 11:13:16 +0100

    publisher atirva

--- library-ejb/src/main/java/com/edu/library/business/publicationManagement/PublisherManagementBusiness.java
+++ library-ejb/src/main/java/com/edu/library/business/publicationManagement/PublisherManagementBusiness.java
@@ -6,42 +6,51 @@ import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
+import com.edu.library.business.exception.BusinessException;
+import com.edu.library.business.exception.ErrorMessages;
 import com.edu.library.data.publicationManagement.PublisherManager;
 import com.edu.library.model.Publisher;
 
 @Stateless
 @LocalBean
 public class PublisherManagementBusiness {
-	
+
 	@EJB
 	private PublisherManager publisherManager;
-	
-	public List<Publisher> getAll(){
-		
-		return null;
+
+	public List<Publisher> getAll() {
+
+		return publisherManager.getAll();
 	}
-	
-	public List<Publisher> search(String p_searchText){
-		
-		return null;
+
+	public List<Publisher> search(String p_searchText) {
+
+		return publisherManager.search(p_searchText);
 	}
-	
-	public Publisher getById(String p_id){
-		
-		return null;
+
+	public Publisher getById(String p_id) {
+
+		return publisherManager.getById(p_id);
 	}
-	
-	public void store(Publisher p_value){
-		
+
+	public void store(Publisher p_value) {
+		Publisher publisher = publisherManager.getById(p_value.getUuid());
+		if(publisher.getName() == null){
+			publisherManager.store(p_value);
+		}else{
+			throw new BusinessException(ErrorMessages.ERROR_CONSTRAINT_VIOLATION);
+		}
 	}
-	
-	public void update(Publisher p_value){
-		
+
+	public void update(Publisher p_value) {
+		publisherManager.getById(p_value.getUuid());
+		publisherManager.update(p_value);
 	}
-	public void remove(String p_id){
+
+	public void remove(String p_id) {
 		Publisher publisher = publisherManager.getById(p_id);
+		publisherManager.remove(publisher);
+		
 	}
-	
-	
 
 }
