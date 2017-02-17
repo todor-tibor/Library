a3fbf9cd96a0a2d6257bbd8e9c9114a7c87b43a2
Sipos Terez <terez.sipos@msg-systems.com>
2017-02-17 12:45:14 +0200

    Author done

---
+++ library-ejb/src/main/java/com/edu/library/business/publicationManagement/AuthorManagementBusiness.java
@@ -0,0 +1,54 @@
+package com.edu.library.business.publicationManagement;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+import com.edu.library.business.exception.BusinessException;
+import com.edu.library.business.exception.ErrorMessages;
+import com.edu.library.data.exception.TechnicalException;
+import com.edu.library.data.publicationManagement.AuthorBean;
+import com.edu.library.model.Author;
+
+@Stateless
+@LocalBean
+public class AuthorManagementBusiness {
+
+	@EJB
+	private AuthorBean dataAcces;
+
+	public List<Author> getAll() {
+		return dataAcces.getAll();
+	}
+
+	public List<Author> search(String p_searchTxt) {
+		return dataAcces.search(p_searchTxt);
+	}
+
+	public Author getById(String p_id) {
+		return dataAcces.getById(p_id);
+	}
+
+	public void store(Author p_value) {
+		try {
+			dataAcces.getByname(p_value.getName());
+			throw new BusinessException(ErrorMessages.ERROR_CONSTRAINT_VIOLATION);
+		} catch (TechnicalException e) {
+			dataAcces.store(p_value);
+		}
+
+	}
+
+	public void update(Author p_user) {
+		dataAcces.update(p_user);
+
+	}
+
+	public void remove(String p_id) {
+		dataAcces.remove(dataAcces.getById(p_id));
+
+	}
+
+}
