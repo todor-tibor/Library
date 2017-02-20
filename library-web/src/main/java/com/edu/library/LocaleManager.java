c7a825a02cbf47935983bb4dc1dba87fa6cb07c0
Sipos Terez <terez.sipos@msg-systems.com>
2017-02-20 16:32:42 +0200

    a bongeszotol kerem le a locale-t
    elonyben reszesul a magyar locale de ha nincs a listaban sem magyar sem
    angol akkor angol a default nyelt

--- library-web/src/main/java/com/edu/library/LocaleManager.java
+++ library-web/src/main/java/com/edu/library/LocaleManager.java
@@ -4,49 +4,49 @@
 package com.edu.library;
 
 import java.io.Serializable;
+import java.util.Iterator;
 import java.util.Locale;
 
 import javax.annotation.PostConstruct;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
 import javax.inject.Named;
 
 /**
  * @author gallb
- *
+ * @author sipost
  */
 
 @Named("localeBean")
 @SessionScoped
 public class LocaleManager implements Serializable {
 
-	/**
-	* 
-	*/
 	private static final long serialVersionUID = -296467386290222310L;
 	private Locale userLocale;
 
 	@PostConstruct
 	private void init() {
+		Iterator<Locale> locales=FacesContext.getCurrentInstance().getExternalContext().getRequestLocales();
+		while(locales.hasNext()){
+			userLocale=locales.next();
+			if("hu".equals(userLocale.getLanguage()) || "HU".equals(userLocale.getCountry())){
+				userLocale=new Locale("hu", "HU");
+				return;
+			}else if("en".equals(userLocale.getLanguage()) || "US".equals(userLocale.getCountry())){
+				userLocale=Locale.ENGLISH;
+				return;
+			}
+		}
 		userLocale=Locale.ENGLISH;
 	}
 
 	public void setUserLocale(Locale userLocale) {
-		System.out.println("locale set.");
+		System.out.println("locale set."+userLocale);
 		this.userLocale = userLocale;
 	}
 
 	public Locale getUserLocale() {
-		System.out.println("locale get.");
+		System.out.println("locale get."+userLocale);
 		return userLocale;
 	}
-
-	/*
-	 * public void changeLocale(ActionEvent actionEvent) { String id =
-	 * actionEvent.getComponent().getId();
-	 * setUserLocale(id.substring(id.lastIndexOf("_")+1)); }
-	 */
 }
