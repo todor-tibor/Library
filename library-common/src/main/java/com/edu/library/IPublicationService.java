768d102f423e39a9196237a901e5279b0acaf648
Sipos Terez <terez.sipos@msg-systems.com>
2017-02-27 16:04:01 +0200

    pagination for Publication

--- library-common/src/main/java/com/edu/library/IPublicationService.java
+++ library-common/src/main/java/com/edu/library/IPublicationService.java
@@ -13,6 +13,15 @@ import com.edu.library.model.Publication;
  *
  */
 public interface IPublicationService extends IService<Publication> {
+
+	public List<Publication> getAll(final int start, final int fin);
+
+	public List<Publication> search(final String text, final int start, final int fin);
+
+	public long countAll();
+
+	public long countSearch(final String title);
+
 	/**
 	 * Filters the data specified by the {@code filter} object. This can have
 	 * one or more filters set. For example: publication title, publisher etc.
@@ -22,5 +31,7 @@ public interface IPublicationService extends IService<Publication> {
 	 *            filtered of a publication object.
 	 * @return List of Publications
 	 */
-	public List<Publication> filterPublication(PublicationFilter filter);
+	public List<Publication> filterPublication(PublicationFilter filter, final int start, final int fin);
+
+	public long countFilter(PublicationFilter filter);
 }
