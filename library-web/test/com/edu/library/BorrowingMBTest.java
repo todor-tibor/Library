c7078de4ab619e5a6876dfb82786e85dcfea6275
gallb <barna.gall@msg-systems.com>
2017-03-02 12:15:33 +0200

    Unit test for Web Bean Store- publication null.

--- library-web/test/com/edu/library/BorrowingMBTest.java
+++ library-web/test/com/edu/library/BorrowingMBTest.java
@@ -1,5 +1,7 @@
 package com.edu.library;
 
+import static org.mockito.Mockito.times;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,10 +14,12 @@ import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.edu.library.model.Borrow;
+import com.edu.library.model.Publication;
+import com.edu.library.util.MessageService;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BorrowingMBTest {
-	
+
 	@InjectMocks
 	BorrowMB borrowingMB;
 
@@ -24,12 +28,24 @@ public class BorrowingMBTest {
 
 	@Test
 	public void testGetAll() {
-		List<Borrow> borr = new ArrayList<>();
-		Borrow bbb=new Borrow();
+		final List<Borrow> borr = new ArrayList<>();
+		final Borrow bbb = new Borrow();
 		borr.add(bbb);
-		Mockito.when(oBorrow.getAll()).thenReturn(borr);
-		List<Borrow> listB = borrowingMB.getAll();
+		Mockito.when(this.oBorrow.getAll()).thenReturn(borr);
+		final List<Borrow> listB = this.borrowingMB.getAll();
 		Assert.assertEquals(listB, borr);
 	}
 
+	@Mock
+	Publication currentPublication = null;
+	@Mock
+	MessageService message;
+
+	@Test
+	public void testStorePublicationNull() {
+		final Borrow bbb = new Borrow();
+		bbb.setPublication(this.currentPublication);
+		this.borrowingMB.store();
+		Mockito.verify(this.borrowingMB.message, times(1)).warn("managedbean.required");
+	}
 }
