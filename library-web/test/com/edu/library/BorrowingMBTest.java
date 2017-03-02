616ce8c00273212e460d7d2b1d497ef1903d007c
gallb <barna.gall@msg-systems.com>
2017-03-02 12:34:54 +0200

    Test borrow store nullchecks.

--- library-web/test/com/edu/library/BorrowingMBTest.java
+++ library-web/test/com/edu/library/BorrowingMBTest.java
@@ -3,6 +3,7 @@ package com.edu.library;
 import static org.mockito.Mockito.times;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Assert;
@@ -13,8 +14,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import com.edu.library.model.Book;
 import com.edu.library.model.Borrow;
 import com.edu.library.model.Publication;
+import com.edu.library.model.User;
 import com.edu.library.util.MessageService;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -37,15 +40,42 @@ public class BorrowingMBTest {
 	}
 
 	@Mock
-	Publication currentPublication = null;
+	Publication currentPublication;
+	@Mock
+	User currentUser;
+	@Mock
+	Date date2;
 	@Mock
 	MessageService message;
 
 	@Test
-	public void testStorePublicationNull() {
+	public void testStoreNull() {
 		final Borrow bbb = new Borrow();
+
+		this.currentPublication = null;
+		this.currentUser = new User();
+		this.date2 = new Date();
 		bbb.setPublication(this.currentPublication);
+		bbb.setUser(this.currentUser);
+		bbb.setBorrowUntil(this.date2);
 		this.borrowingMB.store();
-		Mockito.verify(this.borrowingMB.message, times(1)).warn("managedbean.required");
+
+		this.currentPublication = new Book();
+		this.currentUser = null;
+		this.date2 = new Date();
+		bbb.setPublication(this.currentPublication);
+		bbb.setUser(this.currentUser);
+		bbb.setBorrowUntil(this.date2);
+		this.borrowingMB.store();
+
+		this.currentPublication = new Book();
+		this.currentUser = new User();
+		this.date2 = null;
+		bbb.setPublication(this.currentPublication);
+		bbb.setUser(this.currentUser);
+		bbb.setBorrowUntil(this.date2);
+		this.borrowingMB.store();
+
+		Mockito.verify(this.borrowingMB.message, times(3)).warn("managedbean.required");
 	}
 }
